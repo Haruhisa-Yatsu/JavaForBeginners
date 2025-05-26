af5a44a6f49f68b468abde1784cdd8762a4cce4e
HaruhisaYatsu <yatsu.haruhisa@nsg.gr.jp>
2025-05-26 10:28:54 +0900

    確認問題3.3

--- src/Start.java
+++ src/Start.java
@@ -3,7 +3,8 @@ public class Start {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("ぷろぐらむをかいしします");
+		System.out.println("でぃびじょん");
+		MyFrame frame1 = new RectTest();
 	}
 
 }
