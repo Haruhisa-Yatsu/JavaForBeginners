ef4d9cb2a9a3209ebdf9793d0fc58ab59ab47f80
HaruhisaYatsu <yatsu.haruhisa@nsg.gr.jp>
2025-05-26 11:56:10 +0900

    RectLoopの追加

--- src/Start.java
+++ src/Start.java
@@ -4,7 +4,10 @@ public class Start {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("でぃびじょん");
-		MyFrame frame1 = new RectAndOval();
+		MyFrame frame1 = new RectLoop();
+		
+
+		
 	}
 
 }
