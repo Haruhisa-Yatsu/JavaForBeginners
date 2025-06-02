c226b4f9ca1f900e0f9b83b6a5e87c4b87c1b45c
HaruhisaYatsu <yatsu.haruhisa@nsg.gr.jp>
2025-06-02 11:47:02 +0900

    確認問題 6.2

--- src/Start.java
+++ src/Start.java
@@ -3,8 +3,8 @@ public class Start {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("でぃびじょん");
-		MyFrame frame1 = new Animation();
+		
+		MyFrame frame1 = new DisplayFaceObject();
 		
 
 		
