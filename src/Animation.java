1abde9803fb542723bcdeb30acf22a7d7bdf0901
HaruhisaYatsu <yatsu.haruhisa@nsg.gr.jp>
2025-06-02 10:12:52 +0900

    練習問題5 コメントの追記と変数の整理

--- src/Animation.java
+++ src/Animation.java
@@ -1,9 +1,12 @@
 
 public class Animation extends MyFrame {
 
-	private float _updateSecond = 0.03f;
+	// 処理停止させる秒数
+	private float _sleepSecond = 0.03f;
 
 	public void run() {
+
+		// 表示させたい箇所のコメントアウトを外してね
 
 		//Practice5_1();
 		//Practice5_2();
@@ -13,6 +16,8 @@ public class Animation extends MyFrame {
 		Practice5_EX2();
 	}
 
+	
+	// 練習問題5-1
 	private void Practice5_1() {
 		int y = 200;
 		int x = 30;
@@ -26,7 +31,7 @@ public class Animation extends MyFrame {
 			fillOval(x, y, 100, 100);
 
 			y -= 5;
-			sleep(_updateSecond);
+			sleep(_sleepSecond);
 		}
 		while (x <= 200) {
 			//(1)画面を消す
@@ -37,11 +42,11 @@ public class Animation extends MyFrame {
 			fillOval(x, y, 100, 100);
 
 			x += 5;
-			sleep(_updateSecond);
+			sleep(_sleepSecond);
 		}
-
 	}
 
+	// 練習問題5-2
 	private void Practice5_2() {
 		int y = 30;
 		int x = 30;
@@ -56,7 +61,7 @@ public class Animation extends MyFrame {
 
 			y += 5;
 			x += 5;
-			sleep(_updateSecond);
+			sleep(_sleepSecond);
 		}
 		while (x >= 30) {
 			//(1)画面を消す
@@ -67,7 +72,7 @@ public class Animation extends MyFrame {
 			fillOval(x, y, 100, 100);
 
 			x -= 5;
-			sleep(_updateSecond);
+			sleep(_sleepSecond);
 		}
 		while (y >= 30) {
 			//(1)画面を消す
@@ -79,10 +84,11 @@ public class Animation extends MyFrame {
 
 			y -= 5;
 			x += 5;
-			sleep(_updateSecond);
+			sleep(_sleepSecond);
 		}
 	}
 
+	// 練習問題5-3
 	private void Practice5_3() {
 		int y = 100;
 		int x = 30;
@@ -97,7 +103,7 @@ public class Animation extends MyFrame {
 				fillOval(x, y, 100, 100);
 
 				x += 5;
-				sleep(_updateSecond);
+				sleep(_sleepSecond);
 			}
 			while (x >= 30) {
 				//(1)画面を消す
@@ -108,12 +114,12 @@ public class Animation extends MyFrame {
 				fillOval(x, y, 100, 100);
 
 				x -= 5;
-				sleep(_updateSecond);
+				sleep(_sleepSecond);
 			}
-
 		}
 	}
 
+	// 練習問題5-EX
 	private void Practice5_EX() {
 		int y = 30;
 		int x = 30;
@@ -128,7 +134,7 @@ public class Animation extends MyFrame {
 				fillOval(x, y, 100, 100);
 
 				x += 5;
-				sleep(_updateSecond);
+				sleep(_sleepSecond);
 			}
 			while (y <= 200) {
 				//(1)画面を消す
@@ -139,7 +145,7 @@ public class Animation extends MyFrame {
 				fillOval(x, y, 100, 100);
 
 				y += 5;
-				sleep(_updateSecond);
+				sleep(_sleepSecond);
 			}
 			while (x >= 30) {
 				//(1)画面を消す
@@ -150,7 +156,7 @@ public class Animation extends MyFrame {
 				fillOval(x, y, 100, 100);
 
 				x -= 5;
-				sleep(_updateSecond);
+				sleep(_sleepSecond);
 
 			}
 			while (y >= 30) {
@@ -162,12 +168,14 @@ public class Animation extends MyFrame {
 				fillOval(x, y, 100, 100);
 
 				y -= 5;
-				sleep(_updateSecond);
+				sleep(_sleepSecond);
 
 			}
 		}
 	}
 
+	// 練習問題5-EX2
+	// (おまけ対応)
 	private void Practice5_EX2() {
 		int y = 30;
 		int x = 30;
@@ -191,10 +199,15 @@ public class Animation extends MyFrame {
 				setColor(0, 128, 0);
 				fillOval(x, y, 100, 100);
 
+				// 三項演算子
+				// isRightがTrueなら→へ移動　Falseなら←へ移動
 				x += isRight ? 5 : -5;
-				sleep(_updateSecond);
+				sleep(_sleepSecond);
 			}
+			
+			// 次回移動する方向を反対にする
 			isRight = !isRight;
+			
 			while (y <= targetY) {
 				//(1)画面を消す
 				clear();
@@ -204,12 +217,20 @@ public class Animation extends MyFrame {
 				fillOval(x, y, 100, 100);
 
 				y += 5;
-				sleep(_updateSecond);
+				sleep(_sleepSecond);
 			}
+			
+			// 確認用にある程度下まで行ったら上に戻す
 			targetY += moveDown;
+			if (y > 400) {
+				y = 30;
+				targetY = y + moveDown;
+			}
 		}
 	}
 
+	// 移動方向と現在地を指定すると
+	// 移動すべき位置まで移動しているかどうかを返す関数
 	private boolean IsRight(boolean isRight, int x) {
 		if (isRight) {
 			return x <= 200;
