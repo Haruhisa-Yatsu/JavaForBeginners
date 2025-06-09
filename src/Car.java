d95860fd5d21badc7014a8eac064787d2c0fe2e4
HaruhisaYatsu <yatsu.haruhisa@nsg.gr.jp>
2025-06-09 11:11:53 +0900

    DisplayCars作成

--- src/Car.java
+++ src/Car.java
@@ -1,28 +1,15 @@
 
-public class Car {
-	int x, y, vx, vy;
+public class Car extends Vehicle{
 
 	public Car(int x, int y, int vx, int vy) {
-		this.x = x;
-		this.y = y;
-
-		this.vx = vx;
-		this.vy = vy;
+		super(x, y, vx, vy);
 	}
 
 	public void draw(MyFrame frame) {
-		frame.fillRect(x, y, 40, 20);
-		frame.fillRect(x - 20, y + 20, 80, 20);
-		frame.fillOval(x - 10, y + 40, 20, 20);
-		frame.fillOval(x + 30, y + 40, 20, 20);
+		frame.fillRect(x + 20, y, 40, 20);
+		frame.fillRect(x, y + 20, 80, 20);
+		frame.fillOval(x + 10, y + 40, 20, 20);
+		frame.fillOval(x + 50, y + 40, 20, 20);
 	}
 
-	public void move() {
-		x += vx;
-		y += vy;
-
-		if (x > 450) {
-			x -= 500;
-		}
-	}
 }
