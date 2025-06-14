2d1a97f05996c0de3744e2617b0a879da8ddfd93
HaruhisaYatsu <yatsu.haruhisa@nsg.gr.jp>
2025-06-14 17:22:09 +0900

    PlayerAnimationの追加

--- src/Start.java
+++ src/Start.java
@@ -4,6 +4,6 @@ public class Start {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		
-		MyFrame frame1 = new DisplayCarsTrains();	
+		MyFrame frame1 = new PlayerAnimation();	
 	}
 }
